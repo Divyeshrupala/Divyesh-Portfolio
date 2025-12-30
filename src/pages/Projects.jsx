6e359528beaf1045d9e984b7d975b2cedd7cc124
Divyesh Rupala <divyeshrupala789@gmail.com>
2025-12-30 14:12:09 +0530

    correct error

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
       title: 'MascotsB - IELTS Coaching & Visa Consultancy Platform',
       description: 'A comprehensive frontend solution for an IELTS coaching institute and visa consultancy service, designed to streamline student engagement and exam preparation.',
       technologies: ['HTML', 'CSS', 'JS', 'Google form'],
-      image: '/mascots.png',
+      image: '/public/mascots.png',
       githubUrl: 'https://github.com/Divyeshrupala/MascotsB_new', // Add your GitHub URL
       liveUrl: '#' // Optional: Add live demo URL
     },
